02a768cf8968ab762dac8638c8e885f54b8644e8
RahmaYasser <rahma.y9079@gmail.com>
2021-05-30 17:16:06 +0200

    commit

---
+++ src/Disk.java
@@ -0,0 +1,8 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Disk implements Serializable {
+    public static int availableSpace;
+    public static ArrayList<Boolean> Blocks = new ArrayList<>();
+
+}
---
+++ src/File.java
@@ -0,0 +1,52 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class File implements Serializable {
+    private String name;
+    private String storageDetails;
+    private int allocationType;
+    private ArrayList<Integer> indexes;
+
+
+    public ArrayList<Integer> getIndexes() {
+        return indexes;
+    }
+
+    public void setIndexes(ArrayList<Integer> indexes) {
+        this.indexes = indexes;
+    }
+
+    public File() {
+        indexes = new ArrayList<>();
+    }
+
+    public File(String name, String storageDetails, int allocationType) {
+        this.name = name;
+        this.storageDetails = storageDetails;
+        this.allocationType = allocationType;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStorageDetails(String storageDetails) {
+        this.storageDetails = storageDetails;
+    }
+
+    public void setAllocationType(int allocationType) {
+        this.allocationType = allocationType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStorageDetails() {
+        return storageDetails;
+    }
+
+    public int getAllocationType() {
+        return allocationType;
+    }
+}
